37431bdfb572a971e7338bc8743cd077dd5e5543
hipticem <maamir.hipticem@gmail.com>
2023-11-23 15:59:13 +0100

    page 404

---
+++ frontend/components/Surprise/SurpriseCom.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../../components/Surprise/Surprise.css";
+
+function SurpriseCom() {
+  return (
+    <div>
+      <div className="containerSurprise">
+        <img src="frontend/src/assets/IMAGE404.png" alt="error" />
+        <img
+          src="frontend/src/assets/téléchargement.png"
+          alt="le pere noel n'existe pas"
+          className="imgSurprise"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SurpriseCom;
